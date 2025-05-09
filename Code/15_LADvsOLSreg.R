5258731b19528e68429b4edae52b95fb1031bc8a
fcabrerahz <fcabrerahz@gmail.com>
2025-05-09 13:12:21 -0600

    Update 15_LADvsOLSreg.R

--- Code/15_LADvsOLSreg.R
+++ Code/15_LADvsOLSreg.R
@@ -52,3 +52,61 @@ abline(lad_fit, col = "red", lwd = 2, lty = 2)
 legend("topleft", legend = c("OLS", "LAD (Median)"), col = c("blue", "red"),
        lty = c(1, 2), lwd = 2)
 
+
+###NOW WITH OUTLIERS FOR LARGE X ONLY AND USING OPTIMIZATION:
+
+# Set seed and simulate data
+set.seed(42)
+n <- 100
+x <- runif(n, 0, 10)
+beta_0 <- 2
+beta_1 <- 0.5
+y_true <- beta_0 + beta_1 * x
+noise <- rnorm(n, 0, 1)
+
+# Modify: Add outliers only when x > 8
+outliers <- ifelse(x > 8, sample(c(0, 20), size = n, replace = TRUE, prob = c(0.8, 0.2)), 0)
+
+# Construct y
+y <- y_true + noise + outliers
+data <- data.frame(x = x, y = y)
+
+# --- Visualize the data ---
+plot(x, y, pch = 20, col = "gray", main = "Simulated Data with Localized Outliers")
+abline(beta_0, beta_1, col = "green", lwd = 2, lty = 3)
+
+# --- OLS estimate for comparison ---
+ols_fit <- lm(y ~ x)
+abline(ols_fit, col = "blue", lwd = 2)
+
+# --- Define LAD loss function ---
+LAD_objective <- function(par, x, y) {
+  intercept <- par[1]
+  slope <- par[2]
+  y_hat <- intercept + slope * x
+  sum(abs(y - y_hat))  # LAD: sum of absolute residuals
+}
+
+# --- Run optimization using optim() ---
+initial_guess <- c(0, 0)
+lad_fit <- optim(par = initial_guess,
+                 fn = LAD_objective,
+                 x = x, y = y,
+                 method = "BFGS")
+
+# --- Extract coefficients ---
+lad_coef <- lad_fit$par
+cat("Manual LAD Estimate (via optim):\n")
+print(lad_coef)
+
+# --- Plot LAD fit ---
+abline(a = lad_coef[1], b = lad_coef[2], col = "red", lwd = 2, lty = 2)
+
+# --- Add legend ---
+legend("topleft",
+       legend = c("True line", "OLS", "Manual LAD"),
+       col = c("green", "blue", "red"),
+       lty = c(3, 1, 2),
+       lwd = 2)
+
+

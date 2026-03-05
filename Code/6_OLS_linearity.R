94667e3ce597f0023553974c8f865c5c3f058fa0
Francisco Cabrera <95443429+fcabrerahz@users.noreply.github.com>
2026-03-04 18:56:09 -0600

    Update 6_OLS_linearity.R

--- Code/6_OLS_linearity.R
+++ Code/6_OLS_linearity.R
@@ -1,37 +1,22 @@
 set.seed(123654)
 
-# Generate many values of X
-X <- runif(10000, -2, 2)  # X values drawn from U[-2,2]
-g_X <- X + 1  # Define a function g(X) that is linear in X
+# Simulate data
+n <- 20000
+X <- runif(n, -2, 2)
+gX <- X + 1
+Y <- X^2 + rnorm(n, 0, 0.5)   # nonlinear CEF on purpose
 
-# Linear case: E[Y | X] = 2X
-Y_linear <- 2 * X + rnorm(10000, 0, 0.5)  # Generate Y values
-E_Y_given_X_linear <- tapply(Y_linear, X, mean)  # Conditional expectation
-E_gX_Y_linear <- tapply(g_X * Y_linear, X, mean)  # E[g(X)Y | X]
-gX_E_Y_linear <- g_X * E_Y_given_X_linear  # g(X) * E[Y | X]
+# Create bins to approximate conditional expectations
+bins <- cut(X, breaks = 80)
 
-# Nonlinear case: E[Y | X] = X^2
-Y_nonlinear <- X^2 + rnorm(10000, 0, 0.5)  # Generate Y values
-E_Y_given_X_nonlinear <- tapply(Y_nonlinear, X, mean)
-E_gX_Y_nonlinear <- tapply(g_X * Y_nonlinear, X, mean)
-gX_E_Y_nonlinear <- g_X * E_Y_given_X_nonlinear
+# Conditional expectations
+EY_X  <- tapply(Y, bins, mean)
+EgXY_X <- tapply(gX * Y, bins, mean)
+EgX_X <- tapply(gX, bins, mean)
 
-# Compare the results
-cat("LINEAR CASE:\n")
-print(cor(E_gX_Y_linear, gX_E_Y_linear))  # Should be ~1 (almost perfect)
+# Compute g(X)E[Y|X]
+gX_EY <- EgX_X * EY_X
 
-cat("NONLINEAR CASE:\n")
-print(cor(E_gX_Y_nonlinear, gX_E_Y_nonlinear))  # Should be < 1
-
-plot(E_gX_Y_linear, gX_E_Y_linear, 
-     main = "Scatter Plot of E[g(X)Y | X] vs. g(X)E[Y | X]", 
-     xlab = "E[g(X)Y | X]", 
-     ylab = "g(X)E[Y | X]", 
-     col = "blue", pch = 19)
-
-plot(E_gX_Y_nonlinear, gX_E_Y_nonlinear, 
-     main = "Scatter Plot of E[g(X)Y | X] vs. g(X)E[Y | X]", 
-     xlab = "E[g(X)Y | X]", 
-     ylab = "g(X)E[Y | X]", 
-     col = "blue", pch = 19)
+# Compare numerically
+cor(EgXY_X, gX_EY)
 
